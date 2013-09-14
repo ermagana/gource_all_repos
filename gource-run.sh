10d48ef9747da75ab7822d9bb0e10fca548557ce
Erik Magana <erik@simpleltc.com>
2013-09-13 21:39:28 -0500

    Updated some stuff

--- gource-run.sh
+++ gource-run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gource --key --user-image-dir avatars --seconds-per-day .1 combinedlog.txt --hide dirnames,filenames --logo ../BitBucket/simpleltc-logo.png --loop
+gource --key --user-image-dir avatars --seconds-per-day .15 combinedlog.txt --hide dirnames,filenames --logo ../BitBucket/simpleltc-logo.png --loop --caption-file captions.log --caption-offset -1% --caption-duration 3
