692272ff79ff8f82e6405074f0858d2964e3156d
Erik Magana <erik@simpleltc.com>
2013-12-05 19:08:29 -0600

    updated the run script

--- gource-run.sh
+++ gource-run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-gource --key --user-image-dir avatars --seconds-per-day .15 combinedlog.txt --hide dirnames,filenames --logo ../BitBucket/simpleltc-logo.png --loop --caption-file captions.log --caption-offset -1% --caption-duration 3
+gource --key --user-image-dir avatars --seconds-per-day .15 combinedlog.txt --hide dirnames,filenames --logo ../BitBucket/simpleltc-logo.png --caption-file captions.log --caption-offset -1% --caption-duration 3 --date-format "%B %d, %Y"
+#-f -1280x768 -o erik.ppm
